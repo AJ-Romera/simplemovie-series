7b776a50f4ffe0f6ae23c3a832d2b088ba16e612
AJ-Romera <ajromeraalfonso@gmail.com>
2021-08-04 12:51:06 +0200

    CHNG: Map every Movie and Show them

--- src/components/MovieComponent.js
+++ src/components/MovieComponent.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-function MovieComponent() {
-	const movies = useSelector((state) => state.allMovies.movies);
-	/* const { imdbID, Title } = movies[0]; */
-	return (
-		<div>
-			{/* <header>{Title}</header>
-			<p>{imdbID}</p> */}
-		</div>
-	);
-}
+const MovieComponent = () => {
+	const movies = useSelector((state) => state.allMovies.movies.Search);
+	console.log(movies);
+
+	const renderList = movies.map((movie) => {
+		const { Title, Year, imdbID, Type, Poster } = movie;
+		return (
+			<div>
+				<Link to={`/movie/${imdbID}`}>
+					<p>{Title}</p>
+				</Link>
+				<p>{Year}</p>
+				<p>{imdbID}</p>
+				<p>{Type}</p>
+				<p>{Poster}</p>
+			</div>
+		);
+	});
+
+	return <>{renderList}</>;
+};
 
 export default MovieComponent;
